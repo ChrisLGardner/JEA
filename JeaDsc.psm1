c540bec99d90fca15cdb8b8ff6e9003abf47a43d
Raimund Andree <r.andree@live.com>
2019-11-29 17:04:20 +0100

    Removing AST code

--- JeaDsc.psm1
+++ JeaDsc.psm1
@@ -47,48 +47,6 @@ Function Convert-StringToArrayOfObject($literalString) {
         return $items
     }
 
-    # match:
-    # 1. single string
-    # 2. single hashtable
-    # 3. array of strings and/or hashtables
-    #$predicate = {
-    #    param($ast)
-    #
-    #    if ($ast -is [System.Management.Automation.Language.HashtableAst]) {
-    #        # single hashtable or array item as hashtable
-    #        return ($ast.Parent -is [System.Management.Automation.Language.ArrayLiteralAst]) -or `
-    #        ($ast.Parent -is [System.Management.Automation.Language.CommandExpressionAst])
-    #    }
-    #    elseif ($ast -is [System.Management.Automation.Language.StringConstantExpressionAst]) {
-    #        # array item as string
-    #        if ($ast.Parent -is [System.Management.Automation.Language.ArrayLiteralAst]) {
-    #            return $true
-    #        }
-    #
-    #        do {
-    #            if ($ast.Parent -is [System.Management.Automation.Language.HashtableAst]) {
-    #                # string nested within a hashtable
-    #                return $false
-    #            }
-    #
-    #            $ast = $ast.Parent
-    #        }
-    #        while ( $ast -ne $null )
-    #
-    #        # single string
-    #        return $true
-    #    }
-    #
-    #    return $false
-    #}
-
-    #$rootAst = [System.Management.Automation.Language.Parser]::ParseInput($literalString, [ref] $null, [ref] $null)
-    #$data = $rootAst.FindAll($predicate, $false)
-    #
-    #foreach ($datum in $data) {
-    #    $items += $datum.SafeGetValue()
-    #}
-
     $items = Invoke-Expression -Command $literalString
 
     return $items
